86391253c3a132b7b5d0e4dcd8994ecedf44f7eb
Olga Polishchuk <olgapolichuk@ukr.net>
2024-04-11 15:11:25 +0300

    BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { store, persistor } from "./store";
 import { Provider } from "react-redux";
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Router basename="/BLKS-front-test">
+      <BrowserRouter basename="/BLKS-front-test">
         <App />
-      </Router>
+      </BrowserRouter>
     </PersistGate>
   </Provider>
 );
