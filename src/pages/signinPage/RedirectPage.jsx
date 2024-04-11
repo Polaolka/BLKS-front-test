c957365ea1bad3f148fcfc51e7aa236eb0d52d19
Olga Polishchuk <olgapolichuk@ukr.net>
2024-04-11 14:11:57 +0300

    Update RedirectPage.jsx

--- src/pages/signinPage/RedirectPage.jsx
+++ src/pages/signinPage/RedirectPage.jsx
@@ -4,6 +4,7 @@ import { useParams, useNavigate  } from 'react-router-dom';
 const RedirectPage = () => {
     let navigate = useNavigate();
   const { accessToken, refreshToken } = useParams();
+  console.log("accessToken, refreshToken:", accessToken, refreshToken);
 
   useEffect(() => {
     localStorage.setItem('accessToken', accessToken);
